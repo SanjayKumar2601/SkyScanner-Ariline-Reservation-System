6a8e57acdfb921c70e0f596d3af9b99832e7b046
Sanjay Kumar <18euec143@skcet.ac.in>
2022-11-02 23:42:34 +0530

    updated scurity,logger levels and actuators

--- src/main/java/com/virtusa/SkyScanner/AirlineReservationSystem.java
+++ src/main/java/com/virtusa/SkyScanner/AirlineReservationSystem.java
@@ -2,13 +2,14 @@ package com.virtusa.SkyScanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /** 
  * Airline Reservation System for booking flight tickets
   */
 @SpringBootApplication
-@EnableSwagger2
+//@EnableSwagger2
 public class AirlineReservationSystem {
 
 	public static void main(String[] args) {
